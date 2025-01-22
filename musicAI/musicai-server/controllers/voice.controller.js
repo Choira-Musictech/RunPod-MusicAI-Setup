545536c13915d1970844a74a22af7142390227b5
uday <uday.singh@choira.io>
2025-01-22 18:33:22 +0530

    updated - (voice.controller) - updated the response of voiceController as needed by frontend

--- musicAI/musicai-server/controllers/voice.controller.js
+++ musicAI/musicai-server/controllers/voice.controller.js
@@ -1,5 +1,7 @@
 
 
 exports.voiceController = (req,res) =>{
-    setTimeout(()=>res.json({status:true,link:"https://www.random.com"}),20000)
+    setTimeout(()=>res.json(
+        { status:true, audio_url: "https://files.topmediai.com/aimusic/api/a19bad04-3383-41d3-b581-f7063e0866e7-audio.mp3",
+        image_url: "https://files.topmediai.com/aimusic/api/a19bad04-3383-41d3-b581-f7063e0866e7-image.png" }),20000)
 }
